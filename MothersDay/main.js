4b376afb10a8c22adad78852f432b35a3f13d1c2
jukebx <colegawin@me.com>
2016-05-08 12:17:26 -0500

    Update main.js

--- MothersDay/main.js
+++ MothersDay/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $('.main-nav-list img').resizeOnApproach({
     elementDefault: 100,
     elementClosest: 120,
-    triggerDistance: 50
+    triggerDistance: 110
   });
 });
